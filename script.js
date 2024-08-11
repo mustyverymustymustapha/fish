be1c809f6ab2729953aecbfc754f40e55d0d5908
mustyverymustymustapha <mustaphasatti1@gmail.com>
2024-08-11 14:15:08 +0000

    jrjfjsow

--- script.js
+++ script.js
@@ -10,3 +10,24 @@ function createFish() {
     fishTank.appendChild(fish);
     return fish;
 }
+
+function getRandomColor() {
+    return `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+}
+
+function moveFish(fish) {
+    const newX = Math.random() * 560;
+    const newY = Math.random() * 360;
+    fish.style.left = `${newX}px`;
+    fish.style.top = `${newY}px`;
+}
+
+function changeColor(fish) {
+    fish.style.backgroundColor = getRandomColor();
+}
+
+for (let i = 0; i < numFish; i++) {
+    const fish = createFish();
+    setInterval(() => moveFish(fish), Math.random() * 2000 + 1000);
+    setInterval(() => changeColor(fish), Math.random() * 3000 + 2000);
+}
